f88f63a495a895ac123d3e48de0f63f5920398c6
Parth <pxs4528@mavs.uta.edu>
2023-07-23 19:53:14 -0500

    improved lmao

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,54 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import localFont from '@next/font/local';
 import styles from '../app/page.module.css'
 import Link from "next/link";
+import { useState } from "react";
+
 
 
 const boxing = localFont({ src : '../public/fonts/Boxing-Regular.otf', display: 'swap'});
 const epilogue = localFont({ src : '../public/fonts/Epilogue-Regular.otf', display: 'swap'});
 
-export default function page() {
-
+export default function Page() {
+  const [isHovering, setIsHovering] = useState(false);
+  const handleOver = () => {
+    setIsHovering(true);
+  }
+  const handleLeave = () => {
+    setIsHovering(false);
+  }   
   return (
     <>
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}className={styles.center}>
-        <h1 className={boxing.className}>pip-boy&apos;s Vault</h1>
-        <p className={epilogue.className}>computer science student, he/him</p>
-      </div>
-      <Link href= "https://github.com/pxs4528">
-      <Image style={
-        {
-          position: 'absolute',
-          top: '8%',
-          right: '39%',
-          margin: '10px',
-        }
+    <div className={styles.main}>
+    <div className={styles.container} 
+    onMouseOver={handleOver}
+    onMouseLeave={handleLeave}
+    >
+      {isHovering && (
+        <div>
+      <h1 className={boxing.className}>pip.boy&apos;s vault</h1>
 
-      } src="/../public/github-mark.png" alt="github" width={50} height={50}  />
+          </div>)}
+      {!isHovering && (
+        <div>
+      <h1 className={boxing.className}>parth&apos;s vault</h1>
+      </div>)}
+    </div>
+    <div className={styles.containerP}>
+      <p className={epilogue.className}>software engineer, computer science, uta &apos;25, he/him</p>
+    </div>
+    <div className={styles.imageGit}>
+      <Link href= "https://github.com/pxs4528">
+      <Image src={"/../public/github-mark.png"} alt="github" width={50} height={50}  />
       </Link>
       <Link href= "https://www.linkedin.com/in/parthsharma0310/">
-      <Image style={
-        {
-          position: 'absolute',
-          top: '8%',
-          right: '36%',
-          margin: '10px',
-        }
-
-      } src="/../public/linkedin.png" alt="linkedin" width={50} height={50}  />
+      <Image src={"/../public/linkedin.png"} alt="linkedin" width={50} height={50}  />
       </Link>
-      <div className={styles.image}>
-      <Image src="/favicon.ico" alt="pipboy" width={200} height={200} />
-      </div>
+    </div>
+    </div>
+    
     </>
   )
 }
