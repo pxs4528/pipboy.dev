85ec7e037a45f4241f44831b7226ecdd80a04958
Parth <pxs4528@mavs.uta.edu>
2023-08-23 18:24:54 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,6 @@ export default function Page() {
   }   
   return (
     <>
-      <h1>hello</h1>
     <div className={styles.main}>
       <div className={styles.container} 
       onMouseOver={handleOver}
